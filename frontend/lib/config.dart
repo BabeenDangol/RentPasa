87dee1518b0c0ea8870ca5b02b52fb90c2637682
BabeenDangol <104016877+BabeenDangol@users.noreply.github.com>
2023-07-24 17:35:21 +0545

    Update config.dart

--- frontend/lib/config.dart
+++ frontend/lib/config.dart
@@ -2,11 +2,6 @@
 final url = 'http://192.168.1.72:3000/';
 // final url = 'http://192.168.137.1:3000/';
 
-// final url = 'http://localhost:3000/';
-// final registration = url + "registration";
-// final login = url + 'login';
-// final createProperty = url + 'createProperty';
-
 // final url = 'http://192.168.92.203:3000/';
 
 final registration = url + "registration";
