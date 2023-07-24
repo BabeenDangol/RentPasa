3842d6e9a7e4e3aea63bf6108bbb4aa4a51e457e
BabeenDangol <104016877+BabeenDangol@users.noreply.github.com>
2023-07-24 17:58:01 +0545

    Update config.dart

--- frontend/lib/config.dart
+++ frontend/lib/config.dart
@@ -2,9 +2,12 @@
 final url = 'http://192.168.1.72:3000/';
 // final url = 'http://192.168.137.1:3000/';
 
+<<<<<<< Updated upstream
 // final url = 'http://192.168.92.203:3000/';
 
+=======
 // final url = 'http://192.168.1.65:3000/';
+>>>>>>> Stashed changes
 final registration = url + "registration";
 final login = url + 'login';
 final createBooking = url + 'bookings';
